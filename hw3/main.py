fee677bb82761f74aebe6cc863b80dd7b6773027
munjuhyeok <munjuhyeok@gmail.com>
2020-10-27 14:33:13 +0900

    pixel to (x,y) format

--- hw3/main.py
+++ hw3/main.py
@@ -76,23 +76,23 @@ def warp_image(igs_in, igs_ref, H):
 
     #calculate igs_warp
     igs_warp = np.zeros_like(igs_ref,dtype=np.uint8)
-    i_indices = np.repeat(np.arange(height_ref), width_ref)
-    j_indices = np.tile(np.arange(width_ref), height_ref)
-    indices = np.concatenate((i_indices.reshape(-1,1), j_indices.reshape(-1,1)),axis=1)
+    y_indices = np.repeat(np.arange(height_ref), width_ref)
+    x_indices = np.tile(np.arange(width_ref), height_ref)
+    indices = np.concatenate((x_indices.reshape(-1,1),y_indices.reshape(-1,1)),axis=1)
 
     indices_before_warp = warp_points(indices, H_inv)
-    for i in range(height_ref):
-        for j in range(width_ref):
-            index_before_warp = indices_before_warp[i*width_ref+j]
-            i_before_warp = index_before_warp[0]
-            j_before_warp = index_before_warp[1]
-            if(0<=i_before_warp<height_in and 0<=j_before_warp<width_in):
-                igs_warp[i,j] = igs_in[i_before_warp,j_before_warp]
+    for y in range(height_ref):
+        for x in range(width_ref):
+            index_before_warp = indices_before_warp[y*width_ref+x]
+            x_before_warp = index_before_warp[0]
+            y_before_warp = index_before_warp[1]
+            if(0<=y_before_warp<height_in and 0<=x_before_warp<width_in):
+                igs_warp[y,x] = igs_in[y_before_warp,x_before_warp]
 
     # calculate igs_merge
-    corners = warp_points(np.asarray([[0,0],[0,width_in-1],[height_in-1,0],[height_in-1,width_in-1]]),H)
-    warp_end_height, warp_end_width = corners.max(axis = 0)
-    warp_start_height, warp_start_width = corners.min(axis = 0)
+    corners = warp_points(np.asarray([[0,0],[width_in-1,0],[0,height_in-1],[width_in-1,height_in-1]]),H)
+    warp_end_width, warp_end_height = corners.max(axis = 0)
+    warp_start_width, warp_start_height = corners.min(axis = 0)
 
     pad_up = max(0, -warp_start_height)
     pad_down = max(0, warp_end_height-height_ref)
@@ -101,19 +101,19 @@ def warp_image(igs_in, igs_ref, H):
 
     igs_merge = np.zeros((height_ref+pad_up+pad_down, width_ref+pad_left+pad_right, 3),dtype=np.uint8)
 
-    i_indices = np.repeat(np.arange(warp_start_height,warp_end_height), warp_end_width-warp_start_width)
-    j_indices = np.tile(np.arange(warp_start_width,warp_end_width), warp_end_height-warp_start_height)
-    indices = np.concatenate((i_indices.reshape(-1,1), j_indices.reshape(-1,1)),axis=1)
+    y_indices = np.repeat(np.arange(warp_start_height,warp_end_height), warp_end_width-warp_start_width)
+    x_indices = np.tile(np.arange(warp_start_width,warp_end_width), warp_end_height-warp_start_height)
+    indices = np.concatenate((x_indices.reshape(-1,1),y_indices.reshape(-1,1)),axis=1)
 
     indices_before_warp = warp_points(indices, H_inv)
     
-    for i in range(warp_end_height - warp_start_height):
-        for j in range(warp_end_width - warp_start_width):
-            index_before_warp = indices_before_warp[i*(warp_end_width-warp_start_width)+j]
-            i_before_warp = index_before_warp[0]
-            j_before_warp = index_before_warp[1]
-            if(0<=i_before_warp<height_in and 0<=j_before_warp<width_in):
-                igs_merge[i+max(0,warp_start_height),j+max(0,warp_start_width)] = igs_in[i_before_warp,j_before_warp]
+    for y in range(warp_end_height - warp_start_height):
+        for x in range(warp_end_width - warp_start_width):
+            index_before_warp = indices_before_warp[y*(warp_end_width-warp_start_width)+x]
+            x_before_warp = index_before_warp[0]
+            y_before_warp = index_before_warp[1]
+            if(0<=y_before_warp<height_in and 0<=x_before_warp<width_in):
+                igs_merge[y+max(0,warp_start_height),x+max(0,warp_start_width)] = igs_in[y_before_warp,x_before_warp]
 
     igs_merge[pad_up:height_ref+pad_up,pad_left:width_ref+pad_left] = igs_ref
 
@@ -128,35 +128,36 @@ def rectify(igs, p1, p2):
     H_inv = np.linalg.inv(H)
 
     igs_rec = np.zeros((400,250,3),dtype=np.uint8)
-    height_rec, width_rec, _ = igs_rec.shape
-    i_indices = np.repeat(np.arange(height_rec), width_rec)
-    j_indices = np.tile(np.arange(width_rec), height_rec)
-    indices = np.concatenate((i_indices.reshape(-1,1), j_indices.reshape(-1,1)),axis=1)
+    # height_rec, width_rec, _ = igs_rec.shape
+    # i_indices = np.repeat(np.arange(height_rec), width_rec)
+    # j_indices = np.tile(np.arange(width_rec), height_rec)
+    # indices = np.concatenate((i_indices.reshape(-1,1), j_indices.reshape(-1,1)),axis=1)
 
-    indices_before_warp = warp_points(indices, H_inv)
-    for i in range(height_rec):
-        for j in range(width_rec):
-            index_before_warp = indices_before_warp[i*width_rec+j]
-            i_before_warp = index_before_warp[0]
-            j_before_warp = index_before_warp[1]
-            if(0<=i_before_warp<height_in and 0<=j_before_warp<width_in):
-                igs_rec[i,j] = igs[i_before_warp,j_before_warp]
+    # indices_before_warp = warp_points(indices, H_inv)
+    # for i in range(height_rec):
+    #     for j in range(width_rec):
+    #         index_before_warp = indices_before_warp[i*width_rec+j]
+    #         i_before_warp = index_before_warp[0]
+    #         j_before_warp = index_before_warp[1]
+    #         if(0<=i_before_warp<height_in and 0<=j_before_warp<width_in):
+    #             igs_rec[i,j] = igs[i_before_warp,j_before_warp]
 
-    return igs_rec
+    return warp_image(igs, igs_rec, H)[0]
 
 
 def set_cor_mosaic():
     # TODO ...
-    p_in = np.asarray([[668,678],[675,691],[679,710],[655,958],[692,1066],[826,1342],[573,1363]], dtype=int)
-    p_ref = np.asarray([[671,52],[679,68],[683,92],[656,388],[693,499],[817,744],[579,786]], dtype=int)
+    # (x, y) format
+    p_in = np.asarray([[678,668],[691,675],[710,679],[958,655],[1066,692],[1342,826],[1363,573]], dtype=int)
+    p_ref = np.asarray([[52,671],[68,679],[92,683],[388,656],[499,693],[744,817],[786,579]], dtype=int)
 
     return p_in, p_ref
 
 def set_cor_rec():
     # TODO ...
-
-    c_in = np.asarray([[16,162],[27,275],[259,162],[245,275]], dtype=int)
-    c_ref = np.asarray([[50,50],[50,200],[350,50],[350,200]], dtype=int)
+    # (x, y) format
+    c_in = np.asarray([[162,16],[259,27],[162,259],[259,245]], dtype=int)
+    c_ref = np.asarray([[50,50],[200,50],[50,350],[200,350]], dtype=int)
 
     return c_in, c_ref
 
@@ -164,7 +165,7 @@ def main():
     ##############
     # step 1: mosaicing
     ##############
-
+    
     # read images
     img_in = Image.open('data/porto1.png').convert('RGB')
     img_ref = Image.open('data/porto2.png').convert('RGB')
@@ -189,7 +190,7 @@ def main():
     # save images
     img_warp.save('porto1_warped.png')
     img_merge.save('porto_merged.png')
-
+  
     ##############
     # step 2: rectification
     ##############
